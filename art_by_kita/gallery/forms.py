53b397762f58445622ceda056fb195f05acc9383
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-09-28 12:28:14 +0100

    added a models/forms.py to handle upload of Paintings with cloudinary and pillow to handle upload of images

---
+++ art_by_kita/gallery/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Painting
+
+class PaintingForm(forms.ModelForm):
+    class Meta:
+        model = Painting
+        fields = ['title', 'description', 'dimensions', 'materials', 'image', 'price']
