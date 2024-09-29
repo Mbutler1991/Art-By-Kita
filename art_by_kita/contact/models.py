83ea0d90772b54ac317a73f2efc0132fdc0ef060
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-09-29 13:05:32 +0100

    set up models, forms and views for contact us and thank you pages

--- art_by_kita/contact/models.py
+++ art_by_kita/contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name} () on {self.created.strftime('%m/%d/%Y')}"
