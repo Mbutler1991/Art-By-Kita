d0d6bdbedb4118e33f1c389a95702bff67316767
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-10-03 11:59:48 +0100

    added admin views for gallery

--- art_by_kita/gallery/admin.py
+++ art_by_kita/gallery/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Painting
 
-# Register your models here.
+class PaintingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'dimension', 'materials', 'description', 'price')
+    search_fields = ('name', 'description', 'materials')
