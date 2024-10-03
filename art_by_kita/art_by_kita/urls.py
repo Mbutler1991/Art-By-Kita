0afd4beabcea4133b66e6ffaa741b18c73cdb940
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-10-03 12:14:07 +0100

    added views for gallery app

--- art_by_kita/art_by_kita/urls.py
+++ art_by_kita/art_by_kita/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('contact/', include('contact.urls'))
-    path('gallery/', include('gallery.urls'))
+    path('contact/', include('contact.urls')),
+    path('gallery/', include('gallery.urls')),
 ]
--- art_by_kita/gallery/views.py
+++ art_by_kita/gallery/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Painting
 
-# Create your views here.
+class PaintingListView(ListView):
+    model = Painting
+    template_name = 'gallery/painting_list.html'
+    context_object_name = 'paintings'
+    paginate_by = 10
+
+class PaintingDetailView(DetailView):
+    model = Painting
+    template_name = 'gallery/painting_detail.html'
+    context_object_name = 'painting'
